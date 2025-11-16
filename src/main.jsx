7507ba64a8b756ad54bd36dd653a4f092e0e7777
MIRAVANI SRI S <107063870+miravanisri@users.noreply.github.com>
2025-11-16 20:07:31 +0530

    Fix import path for App.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app.css'
+import './App.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -10,5 +10,5 @@ createRoot(document.getElementById('root')).render(
      <App />
     </BrowserRouter>
    
-  </StrictMode>,
+  </StrictMode>
 )
